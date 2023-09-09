1d1752b492b3ddd1da3dc6950b81d34c7b8d9dee
EscobarLuis <escobarluis77@gmail.com>
2023-09-09 16:23:44 -0300

    Commit inicial del pig_vivero

--- pig_vivero/manage.py
+++ pig_vivero/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pig_grupo9.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pig_vivero.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- pig_vivero/pig_vivero/urls.py
+++ pig_vivero/pig_vivero/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for pig_grupo9 project.
+"""pig_vivero URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+#from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-   
+    # path('admin/', admin.site.urls),
 ]
