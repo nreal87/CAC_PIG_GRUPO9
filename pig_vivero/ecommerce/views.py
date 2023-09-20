643f644a5e57044f1ff1d04e1cb87f743c66561e
Nicolas Real <nicolaser15@hotmail.com>
2023-09-20 18:54:45 -0300

    agregado del filtro como datetime en footer

--- pig_vivero/ecommerce/views.py
+++ pig_vivero/ecommerce/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.templatetags.static import static
+from datetime import datetime
 
 def buscar_productos(cat = "todas"):
     productos_lista = [{"codigo" : "1lr", "nombre" : "lirio rojo", "descripcion" : "Simbolismo: Amor y Seducción", "precio" : "8500", "stock" : "15", "categoria" : "lirio", "imagen" : static('ecommerce/images/lirio_rojo.jpg')},
@@ -13,35 +14,37 @@ def buscar_productos(cat = "todas"):
                               {"codigo" : "1ma", "nombre" : "macetas", "descripcion" : "variedad en colores y modelos", "precio" : "8000", "stock" : "10", "categoria" : "macetas", "imagen" : static('ecommerce/images/maceta_roja.jpg')}, 
                               {"codigo" : "1he", "nombre" : "herramientas", "descripcion" : "Palas, regaderas, mangueras y mas", "precio" : "8000", "stock" : "10", "categoria" : "herramientas", "imagen" : static('ecommerce/images/herramientas.jpg')},]
     if cat == "todas":
-        return {"productos" : productos_lista}
+        return productos_lista
     else:
         lista_seleccion = []
         for producto in productos_lista:
             if(producto["categoria"] == cat):
                 lista_seleccion.append(producto)
         
-        return {"productos" : lista_seleccion}
+        return lista_seleccion
 
 
 
 # Create your views here.
 def index(request):
-    context = {}
+    context = {"ahora":datetime.now}
     return render(request, "index.html", context)
 
 
 def productos(request):
-    context = buscar_productos()
+    context = {"ahora":datetime.now,
+               "productos":buscar_productos()}
     return render(request,"productos.html", context)
 
 
 def producto_categoria(request, categoria):
-    context = buscar_productos(categoria)
+    context = {"ahora":datetime.now,
+               "productos":buscar_productos(categoria)}
     return render(request, "productos.html", context)
 
 
 def login(request):
-    context = {}
+    context = {"ahora":datetime.now}
     return render(request, "login.html", context)
 
 
