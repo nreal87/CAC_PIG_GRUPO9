6ed6d974442f41dd26d4e72e14c6ecbbfe56c0a1
Nicolas Real <nicolaser15@hotmail.com>
2023-10-07 23:31:08 -0300

    django db configs

--- pig_vivero/pig_vivero/settings.py
+++ pig_vivero/pig_vivero/settings.py
@@ -74,12 +74,16 @@ WSGI_APPLICATION = 'pig_vivero.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': "localhost",
+        'PORT': 5432,
+        'NAME': "postgresql_pig_grupo9_db",
+        'USER': "postgres",
+        'PASSWORD': 'postgresqlpass'
+    }
+}
 
 
 # Password validation
