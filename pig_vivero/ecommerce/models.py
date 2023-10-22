0cdd56d5f4ba98ba1b87cb31aac618d2aeef7270
Nicolas Real <nicolaser15@hotmail.com>
2023-10-21 21:45:03 -0300

    models y primer approache n views

--- pig_vivero/ecommerce/models.py
+++ pig_vivero/ecommerce/models.py
@@ -9,23 +9,35 @@ class Cliente(models.Model):
     numero_de_cliente = models.IntegerField(verbose_name="numero de cliente")
     nombre = models.CharField(verbose_name="nombre", max_length=250)
     apellido = models.CharField(verbose_name="apellido", max_length=250)
+    dni = models.IntegerField(verbose_name="DNI")
+
+
+# -------------------------------------------------------------------
+# Clase "Categoria"
+# -------------------------------------------------------------------
+class Categoria(models.Model):
+    nombre = models.CharField(verbose_name='Nombre', max_length=250)
+    descripcion = models.CharField(verbose_name="descripcion del producto", max_length=250)
+
+    def __str__(self):
+        return self.nombre
+    
 
 # -------------------------------------------------------------------
 # Clase "Producto"
 # -------------------------------------------------------------------
 class Producto(models.Model):
     codigo = models.IntegerField(verbose_name="codigo del producto")
+    nombre = models.CharField(verbose_name="nombre del producto",max_length=250)
     descripcion = models.CharField(verbose_name="descripcion del producto", max_length=250)
     cantidad = models.IntegerField(verbose_name="cantidad en stock")
     precio = models.FloatField(verbose_name="precio del producto")
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    imagen = models.ImageField(verbose_name="imagen del producto")
 
-    # # Definimos el constructor e inicializamos los atributos de instancia
-    # def __init__(self, codigo, descripcion, cantidad, precio):
-    #     self.codigo = codigo           # Código 
-    #     self.descripcion = descripcion # Descripción
-    #     self.cantidad = cantidad       # Cantidad disponible (stock)
-    #     self.precio = precio           # Precio 
-
+    def __str__(self):
+        return self.nombre
+    
     # Este método permite modificar un producto.
     def modificar(self, nueva_descripcion, nueva_cantidad, nuevo_precio):
         self.descripcion = nueva_descripcion  # Modifica la descripción
@@ -41,58 +53,6 @@ class Inventario(models.Model):
     fecha_modificacion = models.DateField(verbose_name="fecha de la ultima modificacion")
     productos_stock = models.ManyToManyField(Producto)
 
-    # def __init__(self):
-    #     self.conexion = get_db_connection()
-    #     self.cursor = self.conexion.cursor()
-
-    def agregar_producto(self, codigo, descripcion, cantidad, precio):
-        producto_existente = self.consultar_producto(codigo)
-        if producto_existente:
-            return jsonify({'message': 'Ya existe un producto con ese código.'}), 400
-        nuevo_producto = Producto(codigo, descripcion, cantidad, precio)
-        sql = f'INSERT INTO productos VALUES ({codigo}, "{descripcion}", {cantidad}, {precio});'
-        self.cursor.execute(sql)
-        self.conexion.commit()
-        return jsonify({'message': 'Producto agregado correctamente.'}), 200
-    
-    def consultar_producto(self, codigo):
-        sql = f'SELECT * FROM productos WHERE codigo = {codigo};'
-        self.cursor.execute(sql)
-        row = self.cursor.fetchone()
-        if row:
-            codigo, descripcion, cantidad, precio = row
-            return Producto(codigo, descripcion, cantidad, precio)
-        return None
-    
-    def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio):
-        producto = self.consultar_producto(codigo)
-        if producto:
-            producto.modificar(nueva_descripcion, nueva_cantidad, nuevo_precio)
-            sql = f'UPDATE productos SET descripcion = "{nueva_descripcion}", cantidad = {nueva_cantidad}, precio = {nuevo_precio} WHERE codigo = {codigo};' 
-            self.cursor.execute(sql)
-            self.conexion.commit()
-            return jsonify({'message': 'Producto modificado correctamente.'}), 200
-        return jsonify({'message': 'Producto no encontrado.'}), 404
-    
-    def listar_productos(self):
-        print("listar productos")
-        self.cursor.execute("SELECT * FROM productos")
-        rows = self.cursor.fetchall()
-        productos = []
-        for row in rows:
-            codigo, descripcion, cantidad, precio = row
-            producto = {'codigo': codigo, 'descripcion': descripcion, 'cantidad': cantidad, 'precio': precio}
-            print(f'{codigo}\t{descripcion}\t{cantidad}\t{precio}')
-            productos.append(producto)
-        return jsonify(productos), 200
-    
-    def eliminar_producto(self, codigo):
-        sql = f'DELETE FROM productos WHERE codigo = {codigo};' 
-        self.cursor.execute(sql)
-        if self.cursor.rowcount > 0:
-            self.conexion.commit()
-            return jsonify({'message': 'Producto eliminado correctamente.'}), 200
-        return jsonify({'message': 'Producto no encontrado.'}), 404
     
 # -------------------------------------------------------------------
 # Clase "Carrito"
@@ -103,51 +63,3 @@ class Carrito(models.Model):
     direccion_entrega = models.CharField(verbose_name="direccion de entrega", max_length=250)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     productos_carrito = models.ManyToManyField(Producto)
-
-    # def __init__(self):
-    #     self.conexion = get_db_connection()
-    #     self.cursor = self.conexion.cursor()
-    #     self.items = []
-    
-    def agregar(self, codigo, cantidad, inventario):
-        producto = inventario.consultar_producto(codigo)
-        if producto is None:
-            return jsonify({'message': 'El producto no existe.'}), 404
-        if producto.cantidad < cantidad:
-            return jsonify({'message': 'Cantidad en stock insuficiente.'}), 400
-        
-        for item in self.items:
-            if item.codigo == codigo:
-                item.cantidad += cantidad
-                sql = f'UPDATE productos SET cantidad = cantidad - {cantidad}  WHERE codigo = {codigo};'
-                self.cursor.execute(sql)
-                self.conexion.commit()
-                return jsonify({'message': 'Producto agregado al carrito correctamente.'}), 200
-
-        nuevo_item = Producto(codigo, producto.descripcion, cantidad, producto.precio)
-        self.items.append(nuevo_item)
-        sql = f'UPDATE productos SET cantidad = cantidad - {cantidad}  WHERE codigo = {codigo};'
-        self.cursor.execute(sql)
-        self.conexion.commit()
-        return jsonify({'message': 'Producto agregado al carrito correctamente.'}), 200
-    
-    def quitar(self, codigo, cantidad, inventario):
-        for item in self.items:
-            if item.codigo == codigo:
-                if cantidad > item.cantidad:
-                    return jsonify({'message': 'Cantidad a quitar mayor a la cantidad en el carrito.'}), 400
-                item.cantidad -= cantidad
-                if item.cantidad == 0:
-                    self.items.remove(item)
-                sql = f'UPDATE productos SET cantidad = cantidad + {cantidad} WHERE codigo = {codigo};'
-                self.cursor.execute(sql)
-                self.conexion.commit()
-                return jsonify({'message': 'Producto quitado del carrito correctamente.'}), 200
-        return jsonify({'message': 'El producto no se encuentra en el carrito.'}), 404
-
-    def mostrar(self):
-        productos_carrito = []
-        for item in self.items:
-            producto = {'codigo': item.codigo, 'descripcion': item.descripcion, 'cantidad': item.cantidad, 'precio': item.precio}
-            productos_carrito.append(producto)
-        return jsonify(productos_carrito), 200
--- pig_vivero/ecommerce/views.py
+++ pig_vivero/ecommerce/views.py
@@ -5,34 +5,34 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from django.contrib import messages
+from ecommerce.models import Cliente, Categoria, Producto, Inventario
 
 
 def __buscar_categorias():
-
-    categorias = ["lirio", "frutales","gromineas","herramientas", "macetas", "fuentes", "cactus"]
+    # categorias = ["lirio", "frutales","gromineas","herramientas", "macetas", "fuentes", "cactus"]
+    categorias = Categoria.objects.all()
     return categorias
 
 def __buscar_productos(cat = "todas"):
-
-    productos_lista = [ {"codigo" : "1lr", "nombre" : "lirio rojo", "descripcion" : "Simbolismo: Amor y Seducción", "precio" : "8500", "stock" : "15", "categoria" : "lirio", "imagen" : static('ecommerce/images/lirio_rojo.jpg')},
-                       {"codigo" : "1lr", "nombre" : "lirio rojo", "descripcion" : "Simbolismo: Amor y Seducción", "precio" : "8500", "stock" : "15", "categoria" : "lirio", "imagen" : static('ecommerce/images/lirio_rojo.jpg')},
-                        {"codigo" : "1lb", "nombre" : "lirio blanco", "descripcion" : "Simbolismo: Pureza y Belleza", "precio" : "8000", "stock" : "10", "categoria" : "lirio", "imagen" : static('ecommerce/images/lirio_blanco.jpg')},
-                        {"codigo" : "1ln", "nombre" : "lirio naranja", "descripcion" : "Simbolismo: Simbolismo: Pasión", "precio" : "8000", "stock" : "10", "categoria" : "lirio", "imagen" : static('ecommerce/images/lirio_naranja.jpg')},
-                        {"codigo" : "1fr", "nombre" : "árboles frutales", "descripcion" : "Consultar según la temporada", "precio" : "8000", "stock" : "10", "categoria" : "frutales", "imagen" : static('ecommerce/images/frutales_rojo.jpg')},
-                        {"codigo" : "1ca", "nombre" : "cactus", "descripcion" : "fuentes de éxito personal y laboral", "precio" : "8000", "stock" : "10", "categoria" : "cactus", "imagen" : static('ecommerce/images/cactus_verde.jpg')},
-                        {"codigo" : "1gr", "nombre" : "gromineas", "descripcion" : "plantita espetacular para decorar", "precio" : "8000", "stock" : "10", "categoria" : "gromineas", "imagen" : static('ecommerce/images/gromineas1.jpg')},
-                        {"codigo" : "1fu", "nombre" : "fuentes", "descripcion" : "contamos con distintos modelos", "precio" : "8000", "stock" : "10", "categoria" : "fuentes", "imagen" : static('ecommerce/images/fuente_jardin.jpg')},
-                        {"codigo" : "1ma", "nombre" : "macetas", "descripcion" : "variedad en colores y modelos", "precio" : "8000", "stock" : "10", "categoria" : "macetas", "imagen" : static('ecommerce/images/maceta_roja.jpg')}, 
-                        {"codigo" : "1he", "nombre" : "herramientas", "descripcion" : "Palas, regaderas, mangueras y mas", "precio" : "8000", "stock" : "10", "categoria" : "herramientas", "imagen" : static('ecommerce/images/herramientas.jpg')},]
+    # productos_lista = [ {"codigo" : "1lr", "nombre" : "lirio rojo", "descripcion" : "Simbolismo: Amor y Seducción", "precio" : "8500", "stock" : "15", "categoria" : "lirio", "imagen" : static('ecommerce/images/lirio_rojo.jpg')},
+    #                     {"codigo" : "1lr", "nombre" : "lirio rojo", "descripcion" : "Simbolismo: Amor y Seducción", "precio" : "8500", "stock" : "15", "categoria" : "lirio", "imagen" : static('ecommerce/images/lirio_rojo.jpg')},
+    #                     {"codigo" : "1lb", "nombre" : "lirio blanco", "descripcion" : "Simbolismo: Pureza y Belleza", "precio" : "8000", "stock" : "10", "categoria" : "lirio", "imagen" : static('ecommerce/images/lirio_blanco.jpg')},
+    #                     {"codigo" : "1ln", "nombre" : "lirio naranja", "descripcion" : "Simbolismo: Simbolismo: Pasión", "precio" : "8000", "stock" : "10", "categoria" : "lirio", "imagen" : static('ecommerce/images/lirio_naranja.jpg')},
+    #                     {"codigo" : "1fr", "nombre" : "árboles frutales", "descripcion" : "Consultar según la temporada", "precio" : "8000", "stock" : "10", "categoria" : "frutales", "imagen" : static('ecommerce/images/frutales_rojo.jpg')},
+    #                     {"codigo" : "1ca", "nombre" : "cactus", "descripcion" : "fuentes de éxito personal y laboral", "precio" : "8000", "stock" : "10", "categoria" : "cactus", "imagen" : static('ecommerce/images/cactus_verde.jpg')},
+    #                     {"codigo" : "1gr", "nombre" : "gromineas", "descripcion" : "plantita espetacular para decorar", "precio" : "8000", "stock" : "10", "categoria" : "gromineas", "imagen" : static('ecommerce/images/gromineas1.jpg')},
+    #                     {"codigo" : "1fu", "nombre" : "fuentes", "descripcion" : "contamos con distintos modelos", "precio" : "8000", "stock" : "10", "categoria" : "fuentes", "imagen" : static('ecommerce/images/fuente_jardin.jpg')},
+    #                     {"codigo" : "1ma", "nombre" : "macetas", "descripcion" : "variedad en colores y modelos", "precio" : "8000", "stock" : "10", "categoria" : "macetas", "imagen" : static('ecommerce/images/maceta_roja.jpg')}, 
+    #                     {"codigo" : "1he", "nombre" : "herramientas", "descripcion" : "Palas, regaderas, mangueras y mas", "precio" : "8000", "stock" : "10", "categoria" : "herramientas", "imagen" : static('ecommerce/images/herramientas.jpg')},]
     if cat == "todas":
-        return productos_lista
+        productos = Producto.objects.all()
     else:
-        lista_seleccion = []
-        for producto in productos_lista:
-            if(producto["categoria"] == cat):
-                lista_seleccion.append(producto)
-        return lista_seleccion
-
+        productos = Producto.objects.filter(categoria = cat)
+    productos_lista = []
+    for prod in productos:
+        productos_lista.append({"codigo":prod.codigo, "nombre":prod.nombre, "descripcion":prod.descripcion, "precio":prod.precio, "stock":prod.cantidad, "categoria":prod.categoria, "imagen":prod.imagen})
+    return productos_lista
+    
 
 # Create your views here.
 def secciones(request, seccion = ''):
