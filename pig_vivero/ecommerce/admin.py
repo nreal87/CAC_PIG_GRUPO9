425ef6210eaddc9fb51856a81ec29a44084b9bab
EscobarLuis <escobarluis77@gmail.com>
2023-11-18 06:28:13 -0300

    agrego personalizacion del admin y gestion de usuarios

--- pig_vivero/ecommerce/admin.py
+++ pig_vivero/ecommerce/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from administracion.models import Categoria, Producto
+from django.contrib.auth.models import User
 # Register your models here.
 
-admin.site.register(Categoria)
-admin.site.register(Producto)
+#con esto registramos los modelos que tiene la relacion muchos a muchos
+class ViveroAdminSite(admin.AdminSite):
+    site_header = "Administracion Vivero Los Lirios."
+    site_title = "Administracion Los Lirios"
+    index_title = "Administracion"
+    
+
+vivero_admin = ViveroAdminSite(name = "viveroAdmin")
+vivero_admin.register(User)
+vivero_admin.register(Categoria)
+vivero_admin.register(Producto)
--- pig_vivero/pig_vivero/urls.py
+++ pig_vivero/pig_vivero/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib import admin
+#from django.contrib import admin
+from ecommerce.admin import vivero_admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', vivero_admin.urls),
     path('administracion/',include("administracion.urls")),
     path('',include("ecommerce.urls")),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
